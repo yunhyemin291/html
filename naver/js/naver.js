d803bd89a943d227ca6f9c8726f27f14c6fa207b
yunhyemin <yunhyemin15@gmail.com>
2020-06-17 17:46:10 +0900

    네이버 예제(1)

---
+++ naver/js/naver.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.icon-jr-naver').hover(function(){
+        $(this).toggleClass('icon-jr-naver-hover')
+    })
+    $('.icon-happybean').hover(function(){
+        $(this).toggleClass('icon-happybean-hover')
+    })
+    $('.more-btn').click(function(){
+        
+    })
+})
